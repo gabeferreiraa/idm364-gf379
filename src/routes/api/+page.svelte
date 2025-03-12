cb6c891c71822278c28b0b0b2b3062608c6754f3
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2025-03-12 15:08:53 -0400

    added api route, is working

---
+++ src/routes/api/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	let products = $state('');
+
+	async function load() {
+		const response = await fetch('/api/products');
+		if (response.ok) products = await response.text();
+	}
+
+	$effect(() => {
+		load();
+	});
+</script>
+
+<h1>api page</h1>
+
+{#if products}
+	<p>{products}</p>
+{/if}
